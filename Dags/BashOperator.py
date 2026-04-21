806c96b5bb0bf0516bd1b0a65356d986770ff3b9
venkat-nelluri <venkatnelluri085@gmail.com>
2026-04-21 14:24:55 -0500

    changes21

--- Dags/BashOperator.py
+++ Dags/BashOperator.py
@@ -1,9 +1,5 @@
 from airflow.sdk import dag,task
-<<<<<<< HEAD
 from airflow.operators.bash import BashOperator
-=======
-
->>>>>>> e8504a1ea177ec28c14be2e199e7c12becd43279
 @dag
 def bash_dag():
 
@@ -16,22 +12,10 @@ def bash_dag():
     def second_task():
         print("hello World2")
     
-<<<<<<< HEAD
-=======
-    
-    @task.python
-    def third_task():
-        print("hello World2")
-
-    @task.python
-    def version():
-        print("Version 2")
->>>>>>> e8504a1ea177ec28c14be2e199e7c12becd43279
 
     @task.bash
     def bash_oper():
         return "echo abcdef"
-<<<<<<< HEAD
     
     bah_task = BashOperator(
         task_id = "bash_task",
@@ -44,17 +28,6 @@ def bash_dag():
     bash_task = bah_task
   
     fisrt >> second >> bash >> bah_task
-=======
-
-
-    fisrt = first_task()
-    second = second_task()
-    third = third_task()
-    bash = bash_oper()
-  
-    fisrt >> second >> third >> bash
-
->>>>>>> e8504a1ea177ec28c14be2e199e7c12becd43279
-bash_dag()
+    bash_dag()
 
 
